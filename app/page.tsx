b1df5837aba19c372a47db3664f665fd859f3780
grsurgeon001 <grsurgeon001@gmail.com>
2025-05-01 15:06:55 +0000

    tailwindcss setting  done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="font-bold text-4xl text-red-500 bg-gray-300">
+        Hello World
+      </h1>
     </div>
   );
 }
